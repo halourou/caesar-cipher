8687122563b2edd39c64055c8490f27c6d1716ac
halourou <halourou@gmail.com>
2016-04-03 23:13:42 -0400

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,31 @@
+def caesar_cipher(string, shift)
+	#takes string and int, shifts each character in string by int value, ignores spaces and punctuation in shifting
+	#does not take numbers
+	#shift should not exceed 26
+	letters = string.split("")
+	shifted_string = []
+	punctuation = [",", " ", ".", "!", "?", "'", ";", ":"]
+	letters.each do |letter|
+		if punctuation.include?(letter)
+			shifted_value = letter
+		else
+			if letter.ord > "Z".ord #checking to see if lowercase or uppercase
+				shifted_value = letter.ord + shift
+				if shifted_value > "z".ord #little z as this letter is lowercase
+					shifted_value -= 26 
+				end
+			else #otherwise, assume uppercase
+				shifted_value = letter.ord + shift 
+				if shifted_value > "Z".ord #checking to see if overshot last letter in alphabet
+					shifted_value -= 26 #wrap around back to start of alphabet if we have overshot
+				end
+			end
+			shifted_value = shifted_value.chr
+		end
+		shifted_string.push(shifted_value)
+	end
+	shifted_string.join
+end
+
+puts caesar_cipher("abcd", 1)
+
